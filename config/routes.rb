f601338ba0fc5e74b98ddddb3ba927514bf5ceba
Vivek Nirkhe <vivek.nirkhe@gmail.com>
2016-04-14 18:21:38 +0000

    Updated navbar and user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
   root to: 'pages#index'
-  
-  get 'pages/index'
-
+  #get 'pages/index'
   get 'pages/contact'
-
   get 'pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
