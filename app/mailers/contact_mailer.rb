117719cfdc041da5598b3377fac4f5e60396aac8
Vivek Nirkhe <vivek.nirkhe@gmail.com>
2016-04-15 20:51:03 +0000

    after setting up mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ActionMailer::Base 
+  default to: "vivek@introp.net" 
+   
+  def contact_email(name, email, message) 
+    @name = name 
+    @email = email     @message = message 
+    mail(from: email, subject: 'viveksruby.com Contact Form Message')  
+  end 
+end 
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :articles
+  resources :contacts
   root to: 'pages#index'
   #get 'pages/index'
   get 'pages/contact'
