fbcf10d7e42713dcd776f28d577c5612338b43a4
Vivek Nirkhe <vivek.nirkhe@gmail.com>
2016-04-15 23:09:07 +0000

    added comment partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   resources :contacts
   root to: 'pages#index'
   #get 'pages/index'
